8fc8a120d6f0f52ce4244f23933b39e9f800698f
Franz Fialho <franzfialho@gmail.com>
2020-11-27 14:08:17 -0300

    Comecando a colocar pecas no Tabuleiro

--- Xadrez_Console/tabuleiro/Peca.cs
+++ Xadrez_Console/tabuleiro/Peca.cs
@@ -5,17 +5,19 @@ namespace tabuleiro
 {
     class Peca
     {
-        public Posicao posicao { get; set; }//A peça presica de uma posição;
+        public Posicao posicao { get; set; }//A peça precisa de uma posição;
         public Cor cor { get; protected set; }// Ela pode ser alterada somente pela classe dela e pela SubClasses.
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.tab = tab;
             this.cor = cor;
             this.qteMovimentos = 0;
         }
+
+
     }
 }
